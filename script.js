5d733771ef35aadbe7fc312427b7ba57d80e8344
Kirito-exe <mannuprakash935@gmail.com>
2026-03-27 09:28:42 +0530

    added a colorful option

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 let mainBody = document.querySelector("#maincontent");
-let button= document.querySelector("#gridSize")
-let size = 16
+let button= document.querySelector("#gridSize");
+let size = 16;
+let colorful=true;
 function gridMap(a){
     for (let i=0;i<a;i++){
         let container = document.createElement("div");
         for (let j=0;j<a;j++){
             let box = document.createElement("div");
-            box.setAttribute("style","border:2px black solid;flex:1")
-            box.addEventListener("mouseenter",()=>box.style.backgroundColor="grey")
+            box.setAttribute("style","flex:1")
+            if (colorful) {
+                let red = 255*Math.random();
+                let blue = 255*Math.random();
+                let green= 255*Math.random();
+                box.addEventListener("mouseenter",()=>box.style.backgroundColor=`rgb(${red},${blue},${green})`)
+            } else{box.addEventListener("mouseenter",()=>box.style.backgroundColor="black")}
             container.appendChild(box);
         }
         container.setAttribute("style","display:flex;flex-direction:row;flex:1")
@@ -31,6 +37,13 @@ function gridSize(){
 button.textContent="Grid Size:"+ size;
 gridMap(size)
 function reset(){
+    mainBody.textContent='';
+    colorful=false;
+    gridMap(size);
+}
+function clrfunc(){
+    if (colorful==true) {colorful=false}
+    else {colorful=true}
     mainBody.textContent='';
     gridMap(size);
 }
